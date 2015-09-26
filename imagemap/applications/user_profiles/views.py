3e39356811d1865786f78545c1759b128dcc889e
Camilo Monsalve <camilo@tn3studio.com>
2015-09-26 14:07:07 -0500

    witout viewport and some printable shit

--- imagemap/applications/user_profiles/views.py
+++ imagemap/applications/user_profiles/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from applications.user_profiles.forms import UserProfileCreationForm
+from applications.user_profiles.models import UserProfile
 
-# Create your views here.
+
+@login_required
+def printable_card(request):
+    data = dict()
+    if request.user.is_authenticated():
+        user_profile = UserProfile.objects.get(user__id=request.user.id)
+        user_name = user_profile.user.username
+        data["user_profile_form"] = UserProfileCreationForm(instance=user_profile)
+        data["user_name"] = user_name
+
+    return render_to_response('user_profiles/printable_card.html',
+                              data,
+                              context_instance=RequestContext(request))
--- imagemap/imagemap/urls.py
+++ imagemap/imagemap/urls.py
@@ -17,12 +17,14 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from applications.images.views import index_view, logout_view, image_map_view
+from applications.user_profiles.views import printable_card
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^logout/$', logout_view, name='logout'),
     url(r'^$', index_view, name='index'),
     url(r'^building/$', image_map_view, name='image_map_view'),
+    url(r'^printable-card/$', printable_card, name='printable_card'),
 
     url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT, }),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, }),
